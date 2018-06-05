228d164acc0e715b8bddd922d0356db5fce2ac3c
Masakazu Nomura <hb63598fd7ur5y@softbank.ne.jp>
2018-06-05 12:39:19 +0900

    Initial Commit

---
+++ HealthAccelerated/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthAccelerated
+//
+//  Created by Masakazu on 2018/06/05.
+//  Copyright © 2018 Masakazu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
